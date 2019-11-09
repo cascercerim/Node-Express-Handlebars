682de53fdb8e9170b60ce4e0e09389ff24461dbc
Maria <cascercerim@gmail.com>
2019-11-09 15:31:26 -0500

    committing file path

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Kaylam9!",
+    database: "burgers_db"
+})
+
+connection.connect(function (err) {
+    if (err) throw err;
+});
